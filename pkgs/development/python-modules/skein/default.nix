9101f7b9cb0d7cc1922739f27c9b5e6db69851e7
illustris <rharikrishnan95@gmail.com>
2024-05-08 09:48:05 +0530

    skein: mark as broken

--- pkgs/development/python-modules/skein/default.nix
+++ pkgs/development/python-modules/skein/default.nix
@@ -59,6 +59,6 @@ buildPythonPackage rec {
     maintainers = with maintainers; [ alexbiehl illustris ];
     # https://github.com/NixOS/nixpkgs/issues/48663#issuecomment-1083031627
     # replace with https://github.com/NixOS/nixpkgs/pull/140325 once it is merged
-    broken = lib.traceIf isPy27 "${pname} not supported on ${python.executable}" isPy27;
+    broken = true;
   };
 }
